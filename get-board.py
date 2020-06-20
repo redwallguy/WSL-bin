3bded00e08a55160c12e0f4cb855a81eb07ad5d3
Dev Vibhu Someshwar <contact@devsdigest.com>
2020-06-20 11:59:45 -0400

    Added comment about updated pinterest API

--- get-board.py
+++ get-board.py
@@ -8,6 +8,8 @@ board = sys.argv[1] #for board-specific downloading
 # Usage: Go to the directory you want to download the pinterest board into. Make sure there is an `env.json` file in it with the keys `PINTEREST_API_TOKEN`,
 # `PINTEREST_NEXT_URL`, and `PINTEREST_RATE_LIMIT_REMAINING`, with appropriate values. Then, assuming get-board.py is in your path, `get-board.py <boardname>`
 
+# At the moment, Pinterest API has limited requests to 1/hr
+
 token = ""
 next = None
 rate_limit = 0
